bdae3ab722a131bb43bf270aebcb43793bc2ba7f
Shraddha Suman Mishra <sumanshraddha.mishra@gmail.com>
2024-06-09 21:04:31 +0530

    final

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use("/api/v1/users", require("./routes/userRoute"));
 app.use("/api/v1/transactions", require("./routes/transactionRoutes"));
 
 //static files
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
